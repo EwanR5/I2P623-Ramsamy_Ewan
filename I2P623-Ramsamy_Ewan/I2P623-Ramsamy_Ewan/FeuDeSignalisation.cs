f9a420ebfffbea8deeeff421b7da9e55109ac4ee
EwanR5 <91669749+EwanR5@users.noreply.github.com>
2023-09-26 14:33:17 +0200

    Add files via upload

---
+++ I2P623-Ramsamy_Ewan/I2P623-Ramsamy_Ewan/FeuDeSignalisation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P623_Ramsamy_Ewan
+{
+    internal class FeuDeSignalisation
+    {
+        private uint _couleur;
+        private string _identifiant;
+        private bool _allume;
+
+        public FeuDeSignalisation(string identifiant, uint couleur, bool allume)
+        {
+            _identifiant = identifiant;
+            _couleur = couleur;
+            _allume = allume;
+        }
+
+        public string Identifiant
+        {
+            get { return _identifiant; }
+        }
+        public uint Couleur
+        {
+            get { return _couleur; }
+            set { _couleur = value; }
+        }
+        public bool Allume
+        {
+            get { return _allume; }
+            set { _allume = !value; }
+        }
+
+        public int Change(uint couleur, string identifiant)
+        {
+            if (couleur > 3) 
+            { 
+                couleur = couleur - 3;
+            }
+            if (couleur == 1)
+            {
+                Console.WriteLine("Le feu de singalisation " + identifiant + " est vert");
+                couleur = 2;
+            }
+            else if (couleur == 2)
+            {
+                Console.WriteLine("Le feu de singalisation " + identifiant + " est orange");
+                couleur = 3;
+            }
+            else
+            {
+                Console.WriteLine("Le feu de singalisation " + identifiant + " est rouge");
+                couleur = 1;
+            }
+            return couleur;
+        }
+        public bool Clignote(string identifiant, bool allume) 
+        {
+            if (allume == true)
+            {
+                Console.WriteLine("Le feu de singalisation " + identifiant + " est allumé");
+            }
+            else
+            {
+                Console.WriteLine("Le feu de singalisation " + identifiant + " est éteint");
+            }
+            allume = !allume;
+            return allume;
+        } //pas fini
+        public string AfficherCara()
+        {
+            string etat;
+            string eclairage;
+
+            if (_allume == true)
+            {
+                etat = "allumé";
+            }
+            else
+            {
+                etat = "éteint";
+            }
+            if (_couleur == 1)
+            {
+                eclairage = "vert";
+            }
+            else if (_couleur == 2)
+            {
+                eclairage = "rouge";
+            }
+            else
+            {
+                eclairage = "orange";
+            }
+
+            string chaine = "Etat des feux : \n --------------- \n Le feu de signalisation " + _identifiant + " est " + etat + " et de couleur " + eclairage + " \n \n Changement d'état : \n ---------------";
+            Change(couleur, identifiant);
+
+            return chaine;
+        }
+    }
+}
---
+++ I2P623-Ramsamy_Ewan/I2P623-Ramsamy_Ewan/Program.cs
@@ -0,0 +1,24 @@
+﻿using Animaux;
+using I2P623_Ramsamy_Ewan;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        FeuDeSignalisation Feu1 = new FeuDeSignalisation("1001", 1, true);
+        FeuDeSignalisation Feu2 = new FeuDeSignalisation("007", 1, false);
+
+        Console.WriteLine(Feu1.AfficherCara());
+        Console.WriteLine(Feu2.AfficherCara());
+        Console.ReadLine();
+
+        FeuDeSignalisation[] mesFeux = new FeuDeSignalisation[2];
+
+        mesFeux[0] = Feu1;
+        mesFeux[1] = Feu2;
+
+        Console.WriteLine(mesFeux);
+        Console.ReadLine();
+    }
+}
